9caa5d0a8d4371718ec8401414a2753b805b1de2
Harish291 <choudharyharish355@gmail.com>
2022-03-09 23:33:36 +0530

    Initial commit

---
+++ app/src/main/java/harish/project/salesapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package harish.project.salesapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Making notification bar transparent
+        if (Build.VERSION.SDK_INT >= 31) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        setContentView(R.layout.activity_main);
+    }
+}
